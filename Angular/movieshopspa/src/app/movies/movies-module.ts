ed16723189bd35ac19fdc98f4e75acb2df7b4d0b
jimmyzsh1 <jimmyzsh@gmail.com>
2025-10-30 01:12:47 -0500

    Angular1
    Angular1

---
+++ Angular/movieshopspa/src/app/movies/movies-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieDetails } from './movie-details/movie-details';
+
+
+
+@NgModule({
+  declarations: [
+    MovieDetails
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MoviesModule { }
